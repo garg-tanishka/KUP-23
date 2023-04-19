e481e0cf522f26a1e03aa0aa607ca16926f0b50b
Sans1509 <sanskriti.tiwari@knoldus.com>
2023-04-19 17:15:26 +0530

    Pycharm project of individual dataframe

---
+++ Sanskriti/knolder/src /Utils/main.py
@@ -0,0 +1,7 @@
+from method import create_graph_individual
+
+if __name__ == "__main__":
+    readInput = int(input("Enter a Knolder id: "))
+    print(readInput)
+    user_id = create_graph_individual(readInput)
+
