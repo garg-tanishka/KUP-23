a4a3ea2627a692e4891707c4bd31dc7a8f1baaf8
amanknoldus <aman.srivastava@knoldus.com>
2023-04-27 18:52:32 +0530

    corrected-project-structure

--- Aman-Srivastava/src/Programs/Aman-Srivastava-Project/Project-Practice/main.py
+++ Aman-Srivastava/src/Programs/Aman-Srivastava-Project/Project-Practice/main.py
@@ -1,4 +1,4 @@
-from src.data_extraction.knolder_employee_score_extraction.data_extraction import Plot_Class
+from src.knolder_employee_score_extraction.data_extraction import Plot_Class
 
 if __name__ == "__main__":
     read_input = int(input("Enter Your Knolder ID: "))
--- Aman-Srivastava/src/Programs/Aman-Srivastava-Project/Project-Practice/src/utils/constants.py
+++ Aman-Srivastava/src/Programs/Aman-Srivastava-Project/Project-Practice/src/utils/constants.py
@@ -2,6 +2,5 @@ from pathlib import Path
 
 p = Path(__file__).resolve().parent.parent
 
-method_path = p / "data_extraction"
-files_path = method_path / "csv_data"
+files_path = p / "csv_data"
 dataset_path = files_path / "individual_contribution.csv"
