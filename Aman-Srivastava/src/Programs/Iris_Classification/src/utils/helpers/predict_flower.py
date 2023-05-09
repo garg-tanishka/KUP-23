a3a459d817f863b4b85779ab509d3f48dd2a4d6d
amanknoldus <aman.srivastava@knoldus.com>
2023-05-09 23:51:52 +0530

    implemented-multiple-endpoint-for-train-predict

---
+++ Aman-Srivastava/src/Programs/Iris_Classification/src/utils/helpers/predict_flower.py
@@ -0,0 +1,16 @@
+from src.iris_classification_module.pipeline.iris_classification_problem import train_model_decorator
+from src.utils.helpers.make_prediction import prediction_decorator
+
+
+@train_model_decorator
+class TrainModel:
+
+    def __init__(self, user_input):
+        self.input = user_input
+
+
+@prediction_decorator
+class PredictModel:
+
+    def __init__(self, flower_dimension):
+        self.properties = flower_dimension
